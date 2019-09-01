86ed2416f212fe0c59564850c7e5b8aaf01e5e90
1084931901@qq.com <you44340950>
2019-09-01 17:45:50 +0800

    create application

---
+++ src/test/java/com/example/paasclouddemo/PaascloudDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.paasclouddemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaascloudDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
