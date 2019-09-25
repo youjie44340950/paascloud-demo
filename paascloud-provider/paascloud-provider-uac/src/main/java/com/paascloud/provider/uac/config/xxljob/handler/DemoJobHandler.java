1fcef33481f2907011b88e7c67a83cfd464fcb8a
1084931901@qq.com <you44340950>
2019-09-25 08:09:32 +0800

    docker

--- paascloud-provider/paascloud-provider-uac/src/main/java/com/paascloud/provider/uac/config/xxljob/handler/DemoJobHandler.java
+++ paascloud-provider/paascloud-provider-uac/src/main/java/com/paascloud/provider/uac/config/xxljob/handler/DemoJobHandler.java
@@ -4,6 +4,8 @@ import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.handler.annotation.JobHandler;
 import com.xxl.job.core.log.XxlJobLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
@@ -24,12 +26,14 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class DemoJobHandler extends IJobHandler {
 
+	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Override
 	public ReturnT<String> execute(String param) throws Exception {
-		XxlJobLogger.log("XXL-JOB, Hello World.");
+		logger.info("XXL-JOB, Hello World.");
 
 		for (int i = 0; i < 5; i++) {
-			XxlJobLogger.log("beat at:" + i);
+			logger.info("beat at:" + i);
 			TimeUnit.SECONDS.sleep(2);
 		}
 		return SUCCESS;
