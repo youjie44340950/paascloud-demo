0e63bbf634545d141bc84cff35e6dab9a0cfc83f
1084931901@qq.com <you44340950>
2019-09-26 20:57:22 +0800

    spring cloud config

--- paascloud-gateway/src/main/java/com/paascloud/gateway/GatewayApplication.java
+++ paascloud-gateway/src/main/java/com/paascloud/gateway/GatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.paascloud.gateway","com.paascloud.provider.service.hystrix"})
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"com.paascloud.provider.service"})
 public class GatewayApplication {
